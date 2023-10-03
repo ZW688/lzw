fee923b7351be1ae2533fe9b650336ac70dbc376
linzhenwu <2671197172@qq.com>
2023-10-03 11:03:28 +0800

    dev first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Git!");
         System.out.println("First Commit");
+        System.out.println("branch on dev");
     }
 }
